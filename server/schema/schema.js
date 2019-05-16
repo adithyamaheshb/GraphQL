2ef4ab20b4b1c0884c4787a5757ea211020fa5a5
Adithya Mahesh Bariki <adithyamaheshbariki@gmail.com>
2019-05-15 21:43:40 -0400

    Startedddd GraphQL finally

---
+++ server/schema/schema.js
@@ -0,0 +1,37 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+var books = [
+    {name: 'Name of the Wind', genre: 'Fantasy', id: '1'},
+    {name: 'The Final Empire', genre: 'Fantasy', id: '2'},
+    {name: 'The Long earth', genre: 'Sci-Fi', id: '3'},
+];
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields: () => ({
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString }
+    })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootqueryType',
+    field: {
+        book: {
+            type: BookType,
+            args: { id: { type: GraphQLString }},
+            resolve(parent, args) {
+                // code to get data from db
+
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
