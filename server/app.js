fd77c04a9471a45d8b0852d95d9e3a56fb5eb788
Adithya Mahesh Bariki <adithyamaheshbariki@gmail.com>
2019-05-25 15:48:23 -0400

    connected to mLab database

--- server/app.js
+++ server/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
-
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//connect to mlab database;
+mongoose.connect('mongodb://Adithyamahesh:Adithya.596@ds261616.mlab.com:61616/adb-graphql');
+mongoose.connection.once('open', () => {
+    console.log("connected to database");
+})
 
 app.use("/graphql", graphqlHTTP({
     schema: schema,
