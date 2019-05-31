be2841f8f1dd557ab5931f1ac85a11c477c0cd7c
Adithya Mahesh Bariki <adithyamaheshbariki@gmail.com>
2019-05-30 20:39:47 -0400

    gitIgnored mlab db credentials after listening @syntaxfm podcast :)

--- server/app.js
+++ server/app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+import mLabDbString from './config/keys.dev';
 
 const app = express();
 
 //connect to mlab database;
-mongoose.connect('mongodb://Adithyamahesh:Adithya.596@ds261616.mlab.com:61616/adb-graphql');
+mongoose.connect(`${mLabDbString}`);
 mongoose.connection.once('open', () => {
     console.log("connected to database");
 })
