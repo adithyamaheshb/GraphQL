b8b78f05c0a11cc02b3ed7943ba2d6cea6eab2a5
Adithya Mahesh Bariki <adithyamaheshbariki@gmail.com>
2019-06-08 19:18:19 -0400

    Now I know What is GraphQL:)

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -2,7 +2,16 @@ import React from 'react';
 import { graphql } from 'react-apollo';
 import { getBooksQuery } from './../queries/queries'
 
+//component
+import BookDetails from './BookDetails';
+
 class BookList extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state={
+      selectedBook: null
+    }
+  }
   displayData() {
     var data = this.props.data;
     if(data.loading) {
@@ -11,7 +20,7 @@ class BookList extends React.Component{
     else {
       return data.books.map((book, b) => {
         return (
-          <li key={Math.random()}>{book.name}</li>
+          <li key={Math.random()} onClick={() => this.setState({ selectedBook: book.id })}>{book.name}</li>
         )
       })
     }
@@ -22,6 +31,7 @@ class BookList extends React.Component{
         <ul id="book-list">
             {this.displayData()}
         </ul>
+        <BookDetails selectedBook={this.state.selectedBook}/>
       </div>
     )
   };
